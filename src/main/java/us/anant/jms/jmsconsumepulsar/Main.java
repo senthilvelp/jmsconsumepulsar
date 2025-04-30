17118828ebc999db41ed26e06d2dc7d98197eb0e
Senthilvel Palaniappan <senthilvel.palaniappan@gmail.com>
2025-04-29 22:58:30 -0500

    Added producer and consumer

--- src/main/java/us/anant/jms/jmsconsumepulsar/Main.java
+++ src/main/java/us/anant/jms/jmsconsumepulsar/Main.java
@@ -2,6 +2,7 @@ package us.anant.jms.jmsconsumepulsar;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.jms.Destination;
 import javax.jms.JMSConsumer;
@@ -17,9 +18,7 @@ public class Main
 {
     public static void main( String[] args ) {
     	System.out.println("Calling Main");
-    	final String token = """
-    			eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg0MDA3OTEsImlhdCI6MTc0NTgwODc5MSwiaXNzIjoiZGF0YXN0YXgiLCJzdWIiOiJjbGllbnQ7MWY0ZTNmNWYtOWFhNC00ZGYwLThjMDktZGY2ZjA1Nzg4NjdlO2FXNWtkWE4wY21sbGN3PT07ZGVlOTMzNWU1NCIsInRva2VuaWQiOiJkZWU5MzM1ZTU0In0.AvlGS6pRjsPnzf87DGvUlhbLzBWnEIBrz7omdZfqnxKlSTY9Dn_YFxqYSD_f-djiNN3l5anUXve0zR-t5XxhfpZ9PqG3TDRp5SIkNNzcaPEv-8gBT6Y9m7AvUZ8BjIDffR5Sr55V-P_fpwkr16eNaXQG__u_GhksxNq7qDdTZ-0Wn4YIhdAH-e7O8kyNgdK9y7wKkWivvE75h068kU82nkSNKaLya8zQ5v8VsyDzIUI6Pxyi1EGJVS0PNMw17zctRes8MNGKGjPKHoEz9FgwkkqE2T83oaoqACXG1GfJfBXwSs162nSGNbXEm-PBCc1KMf_Jaji0Gjhb3pJArze5Aw
-    			""";
+    	final String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg0MDA3OTEsImlhdCI6MTc0NTgwODc5MSwiaXNzIjoiZGF0YXN0YXgiLCJzdWIiOiJjbGllbnQ7MWY0ZTNmNWYtOWFhNC00ZGYwLThjMDktZGY2ZjA1Nzg4NjdlO2FXNWtkWE4wY21sbGN3PT07ZGVlOTMzNWU1NCIsInRva2VuaWQiOiJkZWU5MzM1ZTU0In0.AvlGS6pRjsPnzf87DGvUlhbLzBWnEIBrz7omdZfqnxKlSTY9Dn_YFxqYSD_f-djiNN3l5anUXve0zR-t5XxhfpZ9PqG3TDRp5SIkNNzcaPEv-8gBT6Y9m7AvUZ8BjIDffR5Sr55V-P_fpwkr16eNaXQG__u_GhksxNq7qDdTZ-0Wn4YIhdAH-e7O8kyNgdK9y7wKkWivvE75h068kU82nkSNKaLya8zQ5v8VsyDzIUI6Pxyi1EGJVS0PNMw17zctRes8MNGKGjPKHoEz9FgwkkqE2T83oaoqACXG1GfJfBXwSs162nSGNbXEm-PBCc1KMf_Jaji0Gjhb3pJArze5Aw";
     	Map<String, Object> configuration = new HashMap<>();
     	  configuration.put("webServiceUrl", "https://pulsar-gcp-useast1.api.streaming.datastax.com");
     	  configuration.put("brokerServiceUrl", "pulsar+ssl://pulsar-gcp-useast1.streaming.datastax.com:6651");
@@ -29,12 +28,13 @@ public class Main
     	  
     	  try (PulsarConnectionFactory factory = new PulsarConnectionFactory(configuration)) {
 	    	  try (JMSContext context = factory.createContext()) {
-	    	      Destination destination = context.createQueue("persistent://industries/automobiles/*");
-	    	      context.createProducer().send(destination, "text");
+	    	      Destination destination = context.createQueue("persistent://industries/automobiles/vw");
+	    	      context.createProducer().send(destination, UUID.randomUUID().toString());
 	    	      try (JMSConsumer consumer = context.createConsumer(destination)) {
 	    	          String message = consumer.receiveBody(String.class);
 	    	          System.out.println(message);
 	    	      } catch (Exception ex) {
+	    	    	  System.out.println("Consumer Error");
 	    	    	  System.out.println(ex.getLocalizedMessage());
 	    	      }
 	    	  } catch (Exception ex) {
